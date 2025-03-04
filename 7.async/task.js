e5f381c1027aa4153f558e2a7dbc26fe39fd1187
Ilya Gorbunov <in7319@gmail.com>
2025-03-04 22:02:42 +0300

    Седьмое задание. Не тдет последний тест.

---
+++ 7.async/task.js
@@ -0,0 +1,81 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if ((time.length && callback.length)> 0){
+            
+            if (this.alarmCollection.length >0) {
+                for (let i = 1; i < this.alarmCollection.length; i=i+1) {
+                    if (this.alarmCollection[i-1].time === time) {
+                    console.warn('Уже присутствует звонок на это же время');
+                    }
+                }
+            }
+            this.alarmCollection.push({time:time, callback:callback, canCall:true});
+            
+        } else {
+            throw new Error ('Отсутствуют обязательные аргументы');
+        }
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter((element) => element.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        let carrentDate = new Date;
+        let hour = carrentDate.getHours();
+        if (hour < 10) {
+            hour = '0'+hour;
+        }
+        let minute = carrentDate.getMinutes()
+        if (minute < 10) {
+            minute = '0'+minute;
+        }
+        return (hour+':'+minute);
+    }
+
+    start() {
+        if (this.intervalId) {
+            
+        } else {
+            this.intervalId = setInterval(() => {
+                this.alarmCollection.forEach((element) => {
+                    if (element.time === this.getCurrentFormattedTime() && element.canCall) {
+                        element.canCall = false;
+                        element.callback();
+                    }
+                });
+            }, 1000);
+        }
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls(){
+        this.alarmCollection.forEach((element) => element.canCall = true);
+    }
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
+
+//в последнем тесте код обращается сюда:
+//it('будильник должен запускать интервал, который не запустит колбек', (done) => {
+ // clock.addClock("16:45", f => f);
+   /// clock.getCurrentFormattedTime = () => "17:00";
+  //  clock.start();
+
+  //  setTimeout(() => {
+  //    expect(clock.alarmCollection[0].canCall).toBe(true);
+   //   done();
+   // }, 1000);
+ // });
